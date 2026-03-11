2a0d20eac0e6fde38dd5af723c774851e31e03aa
Marcin Duch <s24009@pjwstk.edu.pl>
2026-03-11 11:33:35 +0100

    Input username & displaying

--- LAB01/LAB01/Program.cs
+++ LAB01/LAB01/Program.cs
@@ -1,2 +1,8 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using LAB01;
+
+StatisticsHelper helper = new StatisticsHelper(10);
+
+Console.WriteLine("Enter username: ");
+string username = Console.ReadLine();
+Console.WriteLine("Username is: " + username);
