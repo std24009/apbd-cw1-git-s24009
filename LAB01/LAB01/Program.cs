8b8c7f70fe91c5309156136950d63d46e02e9653
Marcin Duch <s24009@pjwstk.edu.pl>
2026-03-15 18:00:45 +0100

    CalculateAverage function

--- LAB01/LAB01/Program.cs
+++ LAB01/LAB01/Program.cs
@@ -14,5 +14,16 @@ namespace LAB01
             NumberGuesser numberGuesser = new NumberGuesser();
             numberGuesser.TryToGuess();
         }
+
+        void CalculateAverage(int[] values)
+        {
+            int sum = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                sum += values[i];
+            }
+            
+            Console.WriteLine(sum / values.Length);
+        }
     }
 }
